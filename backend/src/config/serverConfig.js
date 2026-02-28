d54eeae02424d18541b15875c804bf653a409054
DabralPriyanshu <priyanshudabral07@gmail.com>
2026-02-28 11:42:20 +0530

    Added cors configurations

--- backend/src/config/serverConfig.js
+++ backend/src/config/serverConfig.js
@@ -3,4 +3,5 @@ import dotenv from "dotenv";
 dotenv.config({ quiet: true });
 export default {
   PORT: process.env.PORT || 3001,
+  FRONTEND_URL: process.env.FRONTEND_URL || "http://localhost:5173",
 };
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,12 @@
 import express from "express";
 import ENV from "./config/serverConfig.js";
 import apiRoutes from "./routes/index.js";
+import cors from "cors";
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded());
+app.use(cors({ origin: ENV.FRONTEND_URL, credentials: true }));
 app.use("/api", apiRoutes);
 app.get("/ping", (req, res) => {
   return res.json({ message: "pong" });
