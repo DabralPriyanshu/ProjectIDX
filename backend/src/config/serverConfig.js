adb769b53cbfc26848f25feeec509fa1f42c2aa4
DabralPriyanshu <priyanshudabral07@gmail.com>
2026-02-28 12:51:43 +0530

    Refactor added react project command in serverconfig

--- backend/src/config/serverConfig.js
+++ backend/src/config/serverConfig.js
@@ -4,4 +4,5 @@ dotenv.config({ quiet: true });
 export default {
   PORT: process.env.PORT || 3001,
   FRONTEND_URL: process.env.FRONTEND_URL || "http://localhost:5173",
+  REACT_PROJECT_COMMAND: process.env.REACT_PROJECT_COMMAND,
 };
--- backend/src/controllers/projectController.js
+++ backend/src/controllers/projectController.js
@@ -2,8 +2,10 @@ import util from "node:util";
 import child_process, { exec } from "node:child_process";
 import fs from "node:fs/promises";
 import uuid4 from "uuid4";
+import ENV from "../config/serverConfig.js";
 
 //the exec function takes callback the util module helps to convert callback into promise
+//exec function help to run any terminal cmd
 const execPromisified = util.promisify(child_process.exec);
 const createProject = async (req, res) => {
   //   steps
@@ -15,10 +17,9 @@ const createProject = async (req, res) => {
   await fs.mkdir(`./projects/${projectId}`);
 
   //now execute command inside that folder
-  const response = await execPromisified(
-    `echo y | npm create vite@latest sandbox -- --template react`,
-    { cwd: `./projects/${projectId}` },
-  );
+  const response = await execPromisified(ENV.REACT_PROJECT_COMMAND, {
+    cwd: `./projects/${projectId}`,
+  });
 
   return res.json({ message: "Project created", data: projectId });
 };
